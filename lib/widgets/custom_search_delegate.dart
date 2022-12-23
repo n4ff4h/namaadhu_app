3afb08a92784ae3fbf62d829989dd92af09de033
Naffah Abdulla Rasheed <mail@naffah.me>
2022-12-23 13:31:10 +0500

    refactor: Separate code inside buildResults and buildSuggestions into a single function

--- lib/widgets/custom_search_delegate.dart
+++ lib/widgets/custom_search_delegate.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:namaadhu_vaguthu/models/atoll.dart';
 import 'package:namaadhu_vaguthu/models/island.dart';
-import 'package:namaadhu_vaguthu/shared/constants.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
   final List<Island> islandList;
@@ -38,30 +37,17 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    List<Island> matchQuery = [];
-    for (var island in islandList) {
-      if (island.islandName.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(island);
-      }
-    }
-
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var island = matchQuery[index];
-        var atoll =
-            atollList.firstWhere((element) => element.id == island.atollNumber);
-
-        return ListTile(
-          title: Text('${atoll.atollAbbreviation}.  ${island.islandName}'),
-        );
-      },
-    );
+    return buildListTiles(context);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    return buildListTiles(context);
+  }
+
+  Widget buildListTiles(BuildContext context) {
     List<Island> matchQuery = [];
+
     for (var island in islandList) {
       if (island.islandName.toLowerCase().contains(query.toLowerCase())) {
         matchQuery.add(island);
