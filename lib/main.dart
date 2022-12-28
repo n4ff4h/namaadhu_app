e5256cbf5c8aed80f45373256979c6199d593299
Naffah Abdulla Rasheed <mail@naffah.me>
2022-12-28 18:07:14 +0500

    feat: Change status bar color and system navigation bar color to kBackgroundColor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:namaadhu_vaguthu/services/data_service.dart';
 import 'package:namaadhu_vaguthu/providers/global_providers.dart';
 import 'package:namaadhu_vaguthu/providers/selected_island_provider.dart';
 import 'package:namaadhu_vaguthu/screens/home_screen.dart';
 import 'package:namaadhu_vaguthu/screens/island_selection_screen.dart';
+import 'package:namaadhu_vaguthu/shared/constants.dart';
 import 'package:namaadhu_vaguthu/shared/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -32,6 +34,13 @@ class NamaadhuVaguthuApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedIsland = ref.watch(selectedIslandProvider);
+
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: kBackgroundColor,
+        systemNavigationBarColor: kBackgroundColor,
+      ),
+    );
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
