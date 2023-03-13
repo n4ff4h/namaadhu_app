18b093679d9a80b2aaac7d2e364da2f7b33198d1
Naffah Abdulla Rasheed <mail@naffah.me>
2023-03-13 12:39:18 +0500

    feat: Setup go_router to navigate between home page and settings page
    Create a shell route with a bottom nav bar which contains a home button and a settings button

---
+++ lib/src/router/app_router.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:namaadhu_app/src/features/home/screens/home_screen.dart';
+import 'package:namaadhu_app/src/features/select_island/screens/select_island_screen.dart';
+import 'package:namaadhu_app/src/features/settings/views/notifications/notification_settings_screen.dart';
+import 'package:namaadhu_app/src/router/scaffold_with_bottom_nav_bar.dart';
+
+// private navigators
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
+enum AppRoute {
+  selectIsland,
+  search,
+  home,
+  settings,
+}
+
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/home',
+    navigatorKey: _rootNavigatorKey,
+    redirect: (context, state) {
+      return;
+    },
+    routes: [
+      GoRoute(
+        path: '/select_island',
+        name: AppRoute.selectIsland.name,
+        pageBuilder: (context, state) => NoTransitionPage(
+          key: state.pageKey,
+          child: const SelectIslandScreen(),
+        ),
+      ),
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) =>
+            ScaffoldWithBottomNavBar(child: child),
+        routes: [
+          GoRoute(
+            path: '/home',
+            parentNavigatorKey: _shellNavigatorKey,
+            name: AppRoute.home.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const HomeScreen(),
+            ),
+          ),
+          GoRoute(
+            path: '/settings',
+            name: AppRoute.settings.name,
+            pageBuilder: (context, state) => NoTransitionPage(
+              key: state.pageKey,
+              child: const NotificationSettingsScreen(),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+});
---
+++ lib/src/router/scaffold_with_bottom_nav_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ionicons/ionicons.dart';
+
+import 'app_router.dart';
+
+class ScaffoldWithBottomNavBar extends StatefulWidget {
+  final Widget child;
+
+  const ScaffoldWithBottomNavBar({super.key, required this.child});
+
+  @override
+  State<ScaffoldWithBottomNavBar> createState() =>
+      _ScaffoldWithBottomNavBarState();
+}
+
+class _ScaffoldWithBottomNavBarState extends State<ScaffoldWithBottomNavBar> {
+  // used for the currentIndex argument of BottomNavigationBar
+  int _selectedIndex = 0;
+
+  void _tap(BuildContext context, int index) {
+    if (index == _selectedIndex) {
+      // If the tab hasn't changed, do nothing
+      return;
+    }
+    setState(() => _selectedIndex = index);
+    if (index == 0) {
+      // Note: this won't remember the previous state of the route
+      // More info here:
+      // https://github.com/flutter/flutter/issues/99124
+      context.goNamed(AppRoute.home.name);
+    } else if (index == 1) {
+      context.goNamed(AppRoute.settings.name);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Ionicons.settings),
+            label: 'Settings',
+          ),
+        ],
+        onTap: (index) => _tap(context, index),
+      ),
+    );
+  }
+}
